46fe97fa7876635aea996a46aef0e7b8727472dd
pinkadotted <subkmrspm@gmail.com>
2023-05-24 16:16:56 +0800

    implemented logic for api call, showing card media

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
-const selectedCategory = 'New'
-
-const Sidebar = () => {
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
   return (
       <Stack
           direction='row'
@@ -15,7 +13,8 @@ const Sidebar = () => {
           }}>
           {categories.map((category) => (
               <button
-              className="category-btn"
+                  className="category-btn"
+                  onClick={() => setSelectedCategory(category.name)}
                   style={{
                       background: category.name ===
                           selectedCategory && '#FC1503',
