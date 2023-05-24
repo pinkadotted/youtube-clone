b9d0551729d6a96e626af8201e971d66864f37e3
pinkadotted <subkmrspm@gmail.com>
2023-05-24 13:39:07 +0800

    sidebar basic layout and styling done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
 import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
