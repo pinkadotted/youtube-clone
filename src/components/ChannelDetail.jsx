c3811164315a7121f517ac3b7af2955641c7a646
pinkadotted <subkmrspm@gmail.com>
2023-05-25 01:17:51 +0800

    finished channel page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {Videos, ChannelCard } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setchannelDetail] = useState(null)
+  const [videos, setvideos] = useState([])
+
+  console.log(channelDetail, videos)
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+      .then((data) => setchannelDetail(data?.items[0]))
+    
+      fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setvideos(data?.items))
+  }, [id])
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(173,0,255,1) 0%, rgba(76,191,255,1) 60%, rgba(0,255,122,1) 100%)',
+          zIndex: 10, height: '300px'
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{mr: {sm: '100px'}}} />
+          <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,7 +5,7 @@ import {VideoCard, ChannelCard} from './'
 const Videos = ({ videos }) => {
   return (
     <Stack direction='row' flexWrap='wrap'
-    justifyContent='start' gap={2}>
+    justifyContent='start' alignItems="start" gap={2}>
       {videos.map((item, idx) => (
           <Box key={idx}>
             {item.id.videoId && <VideoCard video={item} />}
